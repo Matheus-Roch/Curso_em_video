17bd86c290c2981937ffbe5bb960b16e8ef0b198
Matheus_roch <matheus.grocha@aluno.faculdadeimpacta.com.br>
2020-12-26 14:40:15 -0300

    exemplo

---
+++ venv/def.py
@@ -0,0 +1,16 @@
+def boasvindas():
+    print('Ola seja muito bem vindo! sua media é ')
+
+
+def tiraMedia(n1,n2,n3):
+    media = (n1+n2+n3) * 3
+    print(media)
+
+
+
+a = float(input('digite o primero numero para a média: '))
+b = float(input('digite o segundo numero para a média: '))
+c = float(input('digite o terceiro numero para a média: '))
+
+boasvindas()
+tiraMedia(a,b,c)
