36de955ab5e9f4d9e7c428b40b5f539d4fb3f29f
Matheus_roch <matheus.grocha@aluno.faculdadeimpacta.com.br>
2020-12-26 20:37:26 -0300

    up26/12/20

---
+++ ddef.py
@@ -0,0 +1,24 @@
+def bemvindo():
+    print(20* '_-')
+    print("Ola, seja bem vindo ")
+    print(20 * '-_')
+
+def taboada(num):
+
+
+
+def contadonumeros():
+    for i in range (m,15,2):
+
+        print(n)
+
+
+#conta de 1 em q começando de traz
+def contandofinal():
+    for l in range (10,0,-1):
+        print(l)
+
+
+numero = int(input('digite um nuero para saber a taboada: '))
+
+
